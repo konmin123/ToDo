ed3d727874f753af3a10d12720535a96d11e33cf
konmin123 <MindlinKS@yandex.ru>
2022-05-18 23:21:00 +0300

    for start

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'ToDo_api',
+    'ToDo_mod',
 ]
 
 MIDDLEWARE = [
